c2f7f09292863b9c5c1576e55fcfe6eedac7deb5
Stefan Glotzbach <s.glotzbach@orangehive.de>
2023-02-17 08:43:55 +0100

    [IMPROVEMENT] Content elements TCA: added appearance, categories and notes default configuration in type configuration

--- Classes/Utility/ModelTcaUtility.php
+++ Classes/Utility/ModelTcaUtility.php
@@ -164,11 +164,22 @@ class ModelTcaUtility
             ],
             $customFields,
             [
+                '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance',
+                '--palette--;;frames',
+                '--palette--;;appearanceLinks',
+
                 '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language',
                 '--palette--;;language',
+
                 '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access',
                 '--palette--;;hidden',
                 '--palette--;;access',
+
+                '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories',
+                'categories',
+
+                '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes',
+                'rowDescription',
             ]
         );
 
