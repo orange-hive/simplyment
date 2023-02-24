ae7e9e1a53571e9bfc3b7ba224c55475462c6278
Stefan Glotzbach <s.glotzbach@orangehive.de>
2023-02-24 09:28:17 +0100

    [RELEASE] 1.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
             'OrangeHive\\Simplyment\\' => 'Classes/',
         ],
     ],
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
     'author' => 'Stefan Glotzbach',
