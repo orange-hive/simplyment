9619116772253caa51d21da2dd3aa8dd8f48c383
Stefan Glotzbach <s.glotzbach@orangehive.de>
2024-07-15 10:20:28 +0200

    [TASK] Clear complete cache on cache clear for simplyment

--- Classes/Hook/CacheClearHook.php
+++ Classes/Hook/CacheClearHook.php
@@ -3,14 +3,16 @@
 namespace OrangeHive\Simplyment\Hook;
 
 use OrangeHive\Simplyment\Cache\CustomCache;
+use TYPO3\CMS\Core\Service\OpcodeCacheService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class CacheClearHook
 {
 
     public function clearCache($params = [], &$reference = NULL) {
         // Add your code for clearing cache here.
-
         CustomCache::flush();
+        GeneralUtility::makeInstance(OpcodeCacheService::class)->clearAllActive();
     }
 
 }
